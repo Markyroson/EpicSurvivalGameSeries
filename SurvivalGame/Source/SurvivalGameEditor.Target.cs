ac111198031e2008776563cee3cd5f0da2e5e348
Markyroson <Markyroson@users.noreply.github.com>
2016-12-07 11:56:19 -0800

    Improved comments

--- SurvivalGame/Source/SurvivalGameEditor.Target.cs
+++ SurvivalGame/Source/SurvivalGameEditor.Target.cs
@@ -10,10 +10,9 @@ public class SurvivalGameEditorTarget : TargetRules
 		Type = TargetType.Editor;
 	}
 
-	//
-	// TargetRules interface.
-	//
-
+	/*
+	 * TargetRules interface.
+	*/
 	public override void SetupBinaries(
 		TargetInfo Target,
 		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
