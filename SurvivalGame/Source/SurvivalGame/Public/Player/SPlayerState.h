55abefea5d5f60b66271a860c5830f141702908b
Markyroson <Markyroson@users.noreply.github.com>
2016-12-08 11:33:42 -0800

    Added comments

--- SurvivalGame/Source/SurvivalGame/Public/Player/SPlayerState.h
+++ SurvivalGame/Source/SurvivalGame/Public/Player/SPlayerState.h
@@ -15,9 +15,11 @@ class SURVIVALGAME_API ASPlayerState : public APlayerState
 
 	ASPlayerState(const FObjectInitializer& ObjectInitializer);
 
+	/* Number of kills */
 	UPROPERTY(Transient, Replicated)
 	int32 NumKills;
-
+	
+	/* Number of deaths */
 	UPROPERTY(Transient, Replicated)
 	int32 NumDeaths;
 
@@ -28,24 +30,30 @@ class SURVIVALGAME_API ASPlayerState : public APlayerState
 	virtual void Reset() override;
 
 public: 
-
+	/* Add Kill */
 	void AddKill();
 
+	/* Add Death */
 	void AddDeath();
 
 	void ScorePoints(int32 Points);
-
+	
+	/* Set team number */
 	void SetTeamNumber(int32 NewTeamNumber);
-
+	
+	/* Get number of teams */
 	UFUNCTION(BlueprintCallable, Category = "Teams")
 	int32 GetTeamNumber() const;
-
+	
+	/* Get number of kills */
 	UFUNCTION(BlueprintCallable, Category = "Score")
 	int32 GetKills() const;
 
+	/* Get number of deaths */
 	UFUNCTION(BlueprintCallable, Category = "Score")
 	int32 GetDeaths() const;
 
+	/* Get score */
 	UFUNCTION(BlueprintCallable, Category = "Score")
 	float GetScore() const;
 	
